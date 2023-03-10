02661f18bf186759a1c75ee36cb90713cb7fcf14
Emerson Viana <missonzito@outlook.com>
2023-03-10 19:04:45 -0300

    Criação do Listener

--- app/src/main/java/com/FaturamentoApp/myapplication/MainActivity.java
+++ app/src/main/java/com/FaturamentoApp/myapplication/MainActivity.java
@@ -2,15 +2,26 @@ package com.FaturamentoApp.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.NumberPicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String ARQUIVO_MEUS_DADOS = "Meu Arquivo";
     Button mBotaoConfirmar;
     TextView mTexto;
+    NumberPicker mNumberPicker;
+
+    private void adicionarValor(int ano, float valor) {
+        SharedPreferences sharedPreferences = getSharedPreferences(
+            ARQUIVO_MEUS_DADOS, Context.MODE_PRIVATE
+        );
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +30,23 @@ public class MainActivity extends AppCompatActivity {
 
         mBotaoConfirmar =findViewById(R.id.button);
         mTexto = findViewById(R.id.textView);
+        mNumberPicker = findViewById(R.id.numberPicker2);
+
+        mNumberPicker.setMinValue(2000);
+        mNumberPicker.setMaxValue(2022);
+
+        mNumberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                Toast.makeText(MainActivity.this, "Teste do Listener", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         mBotaoConfirmar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
             mTexto.setText("Teste de botao!");
+
             }
         });
     }
